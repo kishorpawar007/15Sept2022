849457a743618ac435fca3bade28b7fe095e7729
Kishor <Kishor@DESKTOP-V5NCMV5>
2022-09-15 10:03:24 +0530

    15th September 2022 update

---
+++ src/main/java/MavenPackagePractice/MavenProjectPractice/App.java
@@ -0,0 +1,13 @@
+package MavenPackagePractice.MavenProjectPractice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
